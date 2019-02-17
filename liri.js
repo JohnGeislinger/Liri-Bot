02e97f77fdc84e22a0432b796cc4496114f2b9b0
Gunslinger <jtgeislinger@gmail.com>
2019-02-17 00:20:37 -0500

    update

--- liri.js
+++ liri.js
@@ -1,21 +1,45 @@
-// require("dotenv").config();
+require("dotenv").config();
 const fs = require("fs");
-// const inquirer = require("inquirer");
 const axios = require("axios");
 const Spotify = require("node-spotify-api");
-// const keys = require("./Liri-Bot/keys");
+const keys = require("/Users/john/Desktop/HW-10/Liri-Bot/keys");
 const moment = require("moment");
 
-const spotify = new Spotify({
-    id: "cf832e5b2c5c4ecba5ff945dd34d30e6",
-    secret: "0adc862fb4e847c39de27cb1a6a90d90"
-});
+let spotify = new Spotify(keys.spotify);
 
-const command = process.argv[2];
-const search = process.argv.slice(3).join(" ");
+let command = process.argv[2];
+let search = process.argv.slice(3).join(" ");
+
+function runCommands(command, search) {
+    switch (command) {
+        case "concert-this":
+            getMyBand(search);
+            break;
+        case "spotify-this-song":
+            getMySpotify(search);
+            break;
+        case "movie-this":
+            getMyMovie(search);
+            break;
+        case "do-what-it-says":
+            fs.readFile("random.txt", "UTF-8", function(error, data) {
+                if (error) {
+                    console.log("Error: " + error);
+                }
+                else {
+                    let choice = data.split(",");
+                    runCommands(choice[0], choice[1]);
+                }
+            });
+            break;
+        default:
+            console.log("Use README.md and follow directions");
+            break;
+    }
+};
 
 function getMyBand(artist) {
-    const concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    let concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(concertURL).then(function(response) {
         let data = response.data[0];
@@ -24,8 +48,8 @@ function getMyBand(artist) {
             "-----------------------------",
             "Artist: " + artist,
             "\nVenue: " + data.venue.name,
-            "\nLocation: " + data.venue.city + ", " + data.venue.country,
-            "\nDate: " + data.datetime,
+            "\nLocation: " + data.venue.city + ", " + data.venue.region,
+            "\nDate: " + moment(data.datetime).format("MM/DD/YYYY"),
             "-----------------------------"
         ].join("\n");
 
@@ -37,55 +61,65 @@ function getMyBand(artist) {
 };
 
 function getMySpotify(item) {
-    spotify.search({ type: "track", query: item, limit: 1 }, function(err, data) {
-        if (err) {
-            return console.log("Error occurred: " + err);
-        }
+    spotify.search({ type: 'track', query: item })
+        .then(function(response) {
+            let data = response.tracks.items[0].album;
+            // console.log(data);
 
-        console.log(data);
-    
-        let songData = [
+            let songData = [
+                "-----------------------------",
+                "Artist: " + data.artists[0].name,
+                "\nSong: " + item,
+                "\nSpotify Link: " + data.artists[0].external_urls.spotify,
+                "\nAlbum: " + data.name,
+                "-----------------------------"
+            ].join("\n\n");
+
+            console.log(songData);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+function getMyMovie(movie) {
+    let movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;
+
+    axios.get(movieURL).then(function(response) {
+        let data = response.data;
+        
+        let movieData = [
             "-----------------------------",
-            "Artist: " + artist,
-            "\nVenue: " + data.venue.name,
-            "\nLocation: " + data.venue.city + ", " + data.venue.country,
-            "\nDate: " + data.datetime,
+            "Title: " + data.Title,
+            "\nYear Produced: " + moment(data.Released, "DD-MMM-YYYY").format("dddd, MMMM Do YYYY"),
+            "\nIMDB Rating: " + data.Ratings[0].Value,
+            "\nRotten Tomatoes Rating: " + data.Ratings[1].Value,
+            "\nProduction Country: " + data.Country,
+            "\nLanguage: " + data.Language,
+            "\nPlot: " + data.Plot,
+            "\nActors: " + data.Actors,
             "-----------------------------"
-        ]
+        ].join("\n");
 
-
+        console.log(movieData);
+    })
+    .catch(function(error) {
+        console.log(error);
     });
 };
 
-if (command === "concert-this") {
-    getMyBand(search);
-}
+runCommands(command, search);
 
-if (command === "spotify-this-song") {
-    getMySpotify(search);
-}
 
-// let runSearch = function (command, search) {
-//     switch (command) {
-//         case "concert-this":
-//             getMyBand(search);
-//         break;
-
-//         case "spotify-this-song":
-//             getMySpotify(search);
-//         break;
-
-//         case "movie-this":
-
-//         break;
-
-//         case "do-what-it-says":
-
-//         break;
-
-//         default:
-//         console.log("LIRI does not understand");
-//     }
+// if (command === "concert-this") {
+//     getMyBand(search);
 // }
 
+// if (command === "spotify-this-song") {
+//     getMySpotify(search);
+// }
+
+// if (command === "movie-this") {
+//     getMyMovie(search);
+// }
 
